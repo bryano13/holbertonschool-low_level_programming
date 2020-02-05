65dba4126e7486fa1d66acebe7c51b5fde28221f
Bryan Ortiz <bryano13@hotmail.com>
2020-02-05 23:01:06 +0000

    4 c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* puts - Prints text
+*
+* return - Always 0, means success          
+*
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
