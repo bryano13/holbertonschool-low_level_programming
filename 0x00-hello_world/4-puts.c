5d1d7ddd0e9d51dc8fcb1bb5c4f7a4e62a84f032
Bryan Ortiz <bryano13@hotmail.com>
2020-02-05 23:32:32 +0000

    4 styled

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* puts - Prints text
-*
-* Return - Always 0 (Success)
-*/
-
+ * main - Entry point
+ *
+ *puts - Prints text
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
