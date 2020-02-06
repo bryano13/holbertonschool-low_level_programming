7146e69d034ae3a434bc713d541a56bc1a1f1b82
Bryan Ortiz <bryano13@hotmail.com>
2020-02-06 22:21:20 +0000

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - print 0123456789 using putchar
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 'z';
+
+	while (n >= 'a')
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+
+	return (0);
+}
