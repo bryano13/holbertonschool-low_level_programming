548e9efc5f4209c091f11998efbf52e3a713baf8
Bryan Ortiz <bryano13@hotmail.com>
2020-02-06 23:55:47 +0000

    10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+*main - print 0-9 separated with commas, using putchar
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int j = '0';
+	int i = '0';
+
+	for(i = '0'; i <='9'; i++)
+	{
+		for(j = '0'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if(!(i == '9' && j == '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}	
+	}
+	putchar('\n');
+
+	return (0);
+}
