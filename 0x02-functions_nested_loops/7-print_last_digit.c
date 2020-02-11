e5eb65538612ce82e3a2c635d6c27cdac82b359d
Bryan Ortiz <bryano13@hotmail.com>
2020-02-11 16:02:55 +0000

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*print_last_digit - prints last digit of an int.
+*
+* @n : checked int
+* Return: Always 0.
+*/
+int print_last_digit(int n)
+{
+	int module = 10 % n;
+
+	if (module < 0)
+	{
+		module = module * -1;
+	}
+	_putchar = (module + '0');
+	return (module);
+}
