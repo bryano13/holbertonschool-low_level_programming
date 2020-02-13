53dd67b498923571744f5049b04c2a60479f881c
Bryan Ortiz <bryano13@hotmail.com>
2020-02-13 20:59:41 +0000

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * print_most_numbers - prints the numbers, from 0 to 9, excluding 2 and 4.
+  *
+  * Return: always 0.
+  *
+  */
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	for(n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
+	}
+}
