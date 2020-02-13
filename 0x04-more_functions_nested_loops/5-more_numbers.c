6eccb85338f21a392ebb1ace8460f19e5b7656cd
Bryan Ortiz <bryano13@hotmail.com>
2020-02-13 21:24:56 +0000

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+  *
+  * more_numbers - prints 10 times the numbers, from 0 to 14.
+  * Return: always 0.
+  *
+  */
+
+void more_numbers(void)
+{
+	int lines;
+	int n;
+
+	for (lines = 0; lines < 10; lines++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n <= 9)
+			{
+				_putchar(n + '0');
+			}
+			else
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
