6edbf358cc67fa4b65bf068d926b20dc3fcda1a5
Bryan Ortiz <bryano13@hotmail.com>
2020-02-13 21:03:31 +0000

    4 new line

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,12 @@ void print_most_numbers(void)
 {
 	int n = 0;
 
-	for(n = 0; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		if (n != 2 && n != 4)
 		{
 			_putchar(n + '0');
 		}
 	}
+	_putchar('\n');
 }
