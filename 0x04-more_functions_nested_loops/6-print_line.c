f3198ae7e44a4a97316d37a08c11aadf9095df0b
Bryan Ortiz <bryano13@hotmail.com>
2020-02-13 22:16:15 +0000

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,11 @@ void print_line(int n)
 {
 	int line;
 
-	if (n > 0)
+	for (line = 0; line <= n; line++)
 	{
-		for (line = 0; line <= n; line++)
+		if (n > 0)
 		{
-			_putchar('_');
+		_putchar('_');
 		}
 	}
 	_putchar('\n');
