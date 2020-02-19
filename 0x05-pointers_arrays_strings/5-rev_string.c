ce122ddd8a4951c2ff0a059f9892f0bee7b590d3
bryano13 <bryano13@hotmail.com>
2020-02-18 21:00:40 -0500

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+*/
+
+void rev_string(char *s)
+{
+	int i, j;
+	char tmp;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		i--;
+	}
+	for (j = i; s[j] >= 0; j--)
+	{
+		tmp = *(s + i);
+		*(s + i) = *(s + j);
+		*(s + j) = tmp;
+	}
+}
