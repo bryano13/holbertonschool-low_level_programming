95f9ab4639f55c5d9c754f1b0e7281aa09973dd0
bryano13 <bryano13@hotmail.com>
2020-02-18 21:25:29 -0500

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*puts2 - function that prints even numbers
+*@str: string
+*
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
