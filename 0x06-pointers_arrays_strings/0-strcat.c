f780a341adb6d3e8f133a75dad62fe8829f57925
Bryan Ortiz <bryano13@hotmail.com>
2020-02-20 20:33:08 +0000

    0 and readme

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcat - This function appends the src string to the dest string.
+ * @dest: is a parameter
+ * @src: is a parameter
+ * Return: Always 0 (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	return (dest);
+}
