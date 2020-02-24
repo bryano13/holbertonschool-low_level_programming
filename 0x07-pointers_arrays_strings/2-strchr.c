9a1f3e3f51fd91e75137dfb670e81517aea0ef13
bryano13 <bryano13@hotmail.com>
2020-02-24 16:42:20 -0500

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,19 @@
 /**
- * _strchr - find and cut
- * @s: array in
- * @c: char to search
- * Return: Always 0.
- */
-
+* _strchr - function that locates a char in string
+* @s: given string
+* @c: character to locate on string
+* Return: return pointer to points a position of char
+*/
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-			return (s + i);
+			return (&s[i]);
+		i++;
 	}
 	return ('\0');
 }
