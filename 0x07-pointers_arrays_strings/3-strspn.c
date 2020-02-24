b8519dc8038e0531b2b415926b650a58a3793088
bryano13 <bryano13@hotmail.com>
2020-02-24 17:13:17 -0500

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int count;
 
 	i = j = count = 0;
-	for (i = 0; (s[i] != '\0') && (s[i] != 32); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
