031208045ab5131bba442cfdfba8e826976ee5f8
bryano13 <bryano13@hotmail.com>
2020-02-24 14:53:08 -0500

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,15 +5,14 @@
  * @n: number of char to copy
  * Return: string dest
 */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
+	i = 0;
 	for (i = 0; i < n; i++)
 	{
-		 dest[i] = src [i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
-
