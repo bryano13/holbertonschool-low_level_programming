594513546e40bb11c2f71bdc1f5b4959086fff90
bryano13 <bryano13@hotmail.com>
2020-02-26 22:47:45 -0500

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+/**
+ * factorial - function that returns the factorial of a given number.
+ * @n: given factorial
+ * Return: Factorial n
+ *
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		n = n * factorial(n - 1);
+		return (n);
+	}
+}
