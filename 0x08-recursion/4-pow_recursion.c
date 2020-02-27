e68ccaf6bdbd80a611ae3f7b4030f4871564bf09
bryano13 <bryano13@hotmail.com>
2020-02-26 22:53:14 -0500

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,26 +1,21 @@
-#include "holberton.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _pow_recursion - function that returns x raised to the power of y.
+ * @x: given number
+ * @y: power of y
+ * Return: value x raised to the power of y
  */
-int main(void)
+int _pow_recursion(int x, int y)
 {
-	int r;
-
-	r = _pow_recursion(1, 10);
-	printf("%d\n", r);
-	r = _pow_recursion(1024, 0);
-	printf("%d\n", r);
-	r = _pow_recursion(2, 16);
-	printf("%d\n", r);
-	r = _pow_recursion(5, 2);
-	printf("%d\n", r);
-	r = _pow_recursion(5, -2);
-	printf("%d\n", r);
-	r = _pow_recursion(-5, 3);
-	printf("%d\n", r);
-	return (0);
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
