493d9ce927c2e356987856d64b898549c3b64167
bryano13 <bryano13@hotmail.com>
2020-03-03 19:32:21 -0500

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,11 +1,34 @@
 #include "holberton.h"
 
 /**
- * _strdup -
- *
- * Return: 
+ * _strdup - Copies a string.
+ * @str: string.
+ * Return: copy of the string.
  */
 char *_strdup(char *str)
 {
+	int i, size;
+	char *s;
 
+	if (str == NULL)
+		return (NULL);
+
+	for (size = 0; str[size] != '\0'; size++)
+	{
+	}
+	if (size != 0)
+	{
+		s = malloc(size * sizeof(char));
+
+		for (i = 0; i < size; i++)
+		{
+			s[i] = str[i];
+		}
+	}
+	else
+		return (NULL);
+	return (s);
 }
+
+
+
