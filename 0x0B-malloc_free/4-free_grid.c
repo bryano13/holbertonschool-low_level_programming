0642d7a65ed5ee321cadd29d8f0771b04ffd66dc
bryano13 <bryano13@hotmail.com>
2020-03-03 23:26:48 -0500

    4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * free_grid -
+ * free_grid - free memory grid
+ * @grid: pointer to pointer (int **)
+ * @height: height (int)
  *
- * Return: 
+ * Return: void
  */
 void free_grid(int **grid, int height)
 {
+	int i = 0;
 
+	while (i < height)
+		free(*(grid + i++));
+
+	free(grid);
 }
