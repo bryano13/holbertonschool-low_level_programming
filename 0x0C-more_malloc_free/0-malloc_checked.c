a012c4a9b858b1a47b6006a8bb9e39f1320d3679
bryano13 <bryano13@hotmail.com>
2020-03-05 13:49:53 -0500

    hippoproject

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Function that returns malloc for int b.
+ * @b: space reserved trough malloc.
+ * Return: a pointer to the new space created.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit (98);
+	return (ptr);
+}
