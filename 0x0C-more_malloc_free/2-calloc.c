51985d5c540dc06f655608fe9ed171ffe884a758
bryano13 <bryano13@hotmail.com>
2020-03-05 17:41:51 -0500

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,11 +1,24 @@
 #include "holberton.h"
-
+#include <stdlib.h>
 /**
- * _calloc -
- *
- * Return: 
+ * _calloc - From malloc creates a calloc function.
+ * @nmemb: Multiplier
+ * @size: size of the type.
+ * Return: str
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *str;
+	unsigned int i;
 
+	if ((size || nmemb) == 0)
+		return (NULL);
+	str = malloc(nmemb * size);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i <= size; i++)
+	{
+		str[i] = 0;
+	}
+	return (str);
 }
