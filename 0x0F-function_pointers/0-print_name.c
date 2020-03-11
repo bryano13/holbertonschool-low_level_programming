444f5533785e5f554554c3f6826df38df8d62e8c
bryano13 <bryano13@hotmail.com>
2020-03-11 14:27:06 -0500

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * print_name - prints name
+ * @name: name parameter
+ * @f: function pointer.
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
