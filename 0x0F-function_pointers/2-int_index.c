44a8d312c2bfbe214faf5eafc775f07e7e1898ef
bryano13 <bryano13@hotmail.com>
2020-03-11 17:52:37 -0500

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,11 +1,30 @@
-#include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * int_index -
- *
- * Return: 
+ * int_index - Counts the number of matches for each fucntion
+ * @cmp: Function pointer
+ * @size: size of the array
+ * @array: an array of integers
+ * Return: -1 or another integer
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int index = 0;
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	/*Count how many times the functions return a value different than cero*/
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			/*Count all the matches*/
+			index++;
+		}
+	}
+	return (index);
 
 }
