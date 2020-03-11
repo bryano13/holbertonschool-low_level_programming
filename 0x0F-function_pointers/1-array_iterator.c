6a1249280a81c748787e90fc884dfee0bddefcd7
bryano13 <bryano13@hotmail.com>
2020-03-11 15:35:16 -0500

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i; /*unsigned int is the same as saying size_t*/
+	size_t i; /*unsigned int is the same as saying size_t*/
 
 	if (array == NULL || size <= 0 || action == NULL)
 		return;
 	/* Iterate inside a for loop to be able to print all numbers of the array*/
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
