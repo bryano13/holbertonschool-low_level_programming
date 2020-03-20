96be3f7e3b3d8995453f80a56581bb3df0511c15
bryano13 <bryano13@hotmail.com>
2020-03-19 22:59:48 -0500

    1!

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * list_len -
+ * list_len - counts the nodes in a single list linked list.
+ * @h: pointer to head node.
  *
- * Return: 
+ * Return: number of nodes.
  */
 size_t list_len(const list_t *h)
 {
+	size_t count = 0;
 
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
