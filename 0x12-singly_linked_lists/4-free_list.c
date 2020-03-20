adfdef04512081ebd9a4fb4ea67248cb7d4d3e6c
bryano13 <bryano13@hotmail.com>
2020-03-19 23:14:24 -0500

    4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,19 @@
 #include "lists.h"
-
 /**
- * free_list -
- *
- * Return: 
+ *free_list - eliminate memory leaks while using lists
+ *@head: pointer to the the list
+  *Return: Nothing
  */
 void free_list(list_t *head)
 {
+	int i = 0;
+	void *tmp;
 
+	for (i = 0; head != NULL; i++)
+	{
+		tmp = head;
+		free(head->str);
+		head = head->next;
+		free(tmp);
+	}
 }
