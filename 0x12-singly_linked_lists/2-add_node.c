4b6306b4e13b1edf3969567116378babdfdd69b2
bryano13 <bryano13@hotmail.com>
2020-03-20 00:19:27 -0500

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,11 +1,38 @@
 #include "lists.h"
 
 /**
- * add_node -
+ * _strlen - function that returns the length of a string.
+ * @s: string
+ * Return: length of string
+ **/
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * add_node - Add a new node to the beginning of the list.
+ * @head: Pointer to the first array.
+ * @str: an array.
  *
- * Return: 
+ * Return: the new node of the list.
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new;
 
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
