c1776c16cdc244b155173f7812120ec703778980
bryano13 <bryano13@hotmail.com>
2020-03-24 20:00:00 -0500

    4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * free_listint -
- *
- * Return: 
+ * free_listint - frees all the list node by node.
+ * @head: pointer to the first node of the SLL.
+ * Return: void.
  */
 void free_listint(listint_t *head)
 {
+	listint_t *tmp;
+	listint_t *store;
 
+	tmp = head;
+
+	while (tmp != NULL)
+	{
+		store = tmp->next;
+		free(tmp);
+		tmp = store;
+	}
 }
