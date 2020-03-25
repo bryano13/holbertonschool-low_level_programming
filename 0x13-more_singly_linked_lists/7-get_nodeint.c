025a337fd93cb73329a31f416a9d05604e267b23
bryano13 <bryano13@hotmail.com>
2020-03-24 22:58:25 -0500

    7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,29 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -
+ * get_nodeint_at_index - finds a node.
+ * @index: number of the node to find.
+ * @head: Pointer to the first node.
  *
- * Return: 
+ * Return: the node found.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *tmp;
+	unsigned int count = 0;
 
+	tmp = head;
+
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		count++;
+		if (count == index)
+		{
+			if (tmp == NULL)
+				return (NULL);
+			return (tmp);
+		}
+	}
+	return (NULL);
 }
