e76d3dfbbfb7f788100d12319745d82686227fdc
bryano13 <bryano13@hotmail.com>
2020-03-24 21:10:03 -0500

    6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,5 +7,17 @@
  */
 int pop_listint(listint_t **head)
 {
+	/*Extract the integer value from the head node and then delete that first node*/
 
+	listint_t *tmp;
+	int n;
+
+	if (head == NULL)
+		return (0);
+
+	tmp = *head;
+	n = tmp->n;
+	*head = tmp->next;
+	free(tmp);
+	return (n);
 }
