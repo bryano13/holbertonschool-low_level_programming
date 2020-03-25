1d07071941e37727077d85bebc23b21dcb9a8b0c
bryano13 <bryano13@hotmail.com>
2020-03-24 20:34:09 -0500

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
- *
- * Return: 
+ * free_listint2 - Frees a SLL.
+ * @head: double pointer.
+ * Return: void.
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *tmp;
 
+	if (*head != NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		tmp  = *head;
+		*head = tmp->next;
+		free(tmp);
+	}
 }
