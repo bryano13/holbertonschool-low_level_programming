dae12578121505f79d0606ef0c4203b14357a08c
bryano13 <bryano13@hotmail.com>
2020-03-24 23:38:47 -0500

    9d

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,11 +1,45 @@
-#include "lists.h"
-
+include "lists.h"
 /**
- * insert_nodeint_at_index -
- *
- * Return: 
+ * insert_nodeint_at_index - add a integer node to the end.
+ * @head: head of the list
+ * @idx: the position of the new node
+ * @n: the integer value of the new node
+ * Return: the head of list
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	unsigned int i = 0;
+	listint_t *ant, *new, *temp;
 
+	if (head == NULL)
+		return (0);
+
+	temp = *head;
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	for (i = 0; i < idx; i++)
+	{
+		if (temp)
+		{
+			ant = temp;
+			temp = temp->next;
+		}
+		else
+			return (NULL);
+	}
+	ant->next = new;
+	new->next = temp;
+
+	return (new);
 }
