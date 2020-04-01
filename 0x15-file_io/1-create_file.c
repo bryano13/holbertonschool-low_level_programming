b5453644892fa9e7d82d6746160efb819e9b5494
bryano13 <bryano13@hotmail.com>
2020-03-31 23:19:42 -0500

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,31 @@
 #include "holberton.h"
-
 /**
- * create_file -
- *
- * Return: 
+ * create_file - function that creates a file.
+ * @filename: file to create
+ * @text_content: file content
+ * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
+	int open_f, write_f, len = 0;
 
+	if (filename == NULL)
+		return (-1);
+	open_f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (open_f == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	while (text_content[len] != '\0')
+		len++;
+	write_f = write(open_f, text_content, len);
+
+	if (write_f == -1)
+	{
+		return (-1);
+	}
+	close(open_f);
+	return (1);
 }
