02e10212bfbcf54096773a2fc7d38ee149da1ffb
bryano13 <bryano13@hotmail.com>
2020-05-02 01:32:51 -0500

    0-1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <lists.h>
+#include "lists.h"
 /**
   * print_dlistint - prints n structures
   * @h: head pointer
@@ -9,7 +9,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
