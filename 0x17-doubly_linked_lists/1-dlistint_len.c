061cf6bbf0b2f2bd5f2cee28c69483cb0d54fb49
bryano13 <bryano13@hotmail.com>
2020-05-02 01:52:20 -0500

    1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
- *
- * Return: 
+ * dlistint_len - count num of elements
+ * @h: head pointer
+ * Return: count
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t count = 0;
 
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
