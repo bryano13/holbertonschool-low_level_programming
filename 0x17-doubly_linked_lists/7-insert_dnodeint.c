68fcdbe7a1ba2d69555e3125af5e39914bc73592
bryano13 <bryano13@hotmail.com>
2020-05-03 01:46:20 -0500

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,6 +15,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
+	if (*h == NULL)
+		return (NULL);
 	tranv = *h;
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
