78708f738abab29a5060963178741610169f2d43
bryano13 <bryano13@hotmail.com>
2020-05-02 21:26:33 -0500

    4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,11 +7,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
+	if (head == NULL)
+		return (NULL);
+
 	while (head->next != NULL)
 	{
 		head = head->next;
 		free(head->prev);
 	}
-	free(head->next);
 	free(head);
 }
