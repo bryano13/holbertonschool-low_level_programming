5f9a82b7db291fe65421ce90089c563ba86d96f2
bryano13 <bryano13@hotmail.com>
2020-05-03 00:01:28 -0500

    6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
- *
- * Return: 
+ * sum_dlistint - adds all numbers in the sll
+ * @head: Head pointer
+ * Return: sum of all integers of the sll.
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+	int count = 0;
 
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum = head->n + sum;
+		head = head->next;
+		count++;
+	}
+	if (count == 0)
+		return (0);
+	return (sum);
 }
