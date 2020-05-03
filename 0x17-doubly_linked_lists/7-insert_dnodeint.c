0d96baab2a26d0157502627d762272508ee96cf7
bryano13 <bryano13@hotmail.com>
2020-05-03 01:45:19 -0500

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 
-	if (idx == 0 && *h != NULL)
+	if (idx == 0)
 	{
 		new->next = *h;
 		new->prev = NULL;
