dc15b44bb62b94bddcb8b3a20170c5a9db399b7d
bryano13 <bryano13@hotmail.com>
2020-05-02 22:53:21 -0500

    5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,23 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - finds a node
+ * @head: pointer to head of sll
+ * @index: node index
+ * Return: node found.
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *tranv = NULL;
+	size_t count = 0;
 
+	tranv = head;
+	while (tranv->next != NULL)
+	{
+		if (count == index)
+			return (tranv);
+		tranv = tranv->next;
+		count++;
+	}
+	return (NULL);
 }
